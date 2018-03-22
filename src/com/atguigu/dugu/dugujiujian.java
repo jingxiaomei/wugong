f7cbe88ff414f779d978767985fc35af35276d80
陈源 <1094822455@qq.com>
2018-03-22 14:13:17 +0800

    "打狗棍法"

---
+++ src/com/atguigu/dugu/dugujiujian.java
@@ -0,0 +1,10 @@
+package com.atguigu.dugu;
+
+public class dugujiujian {
+	
+	public static void main(String[] args) {
+		System.out.println("打二狗第一式：棒打狗头");
+		System.out.println("打二狗第二式：打狗无漏");
+		System.out.println("打二狗第三式：反截狗臀");
+	}
+}
